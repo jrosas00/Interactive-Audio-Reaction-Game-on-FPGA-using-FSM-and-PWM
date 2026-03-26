18d41acf1b0f5e671073fb46e96289b6fc1eb7c0
jrosas00 <jamesarosas101400@gmail.com>
2026-03-26 12:22:17 -0700

    Create Top_tb.v testbench for button press simulation
    This file serves as a testbench for simulating button presses and observing outputs.

---
+++ Top_tb.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: CSULB
+// Engineers: Michelle Herrera-Cuen, James Rosas
+// 
+// Create Date: 03/26/2026 11:58:45 AM
+// Module Name: Top_tb
+// Project Name: Final Project
+// Description: Testbench to test button presses
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top_tb(
+
+    ); 
+    /generate clock
+    simulate button presses
+    observe outputs/
+
+endmodule
