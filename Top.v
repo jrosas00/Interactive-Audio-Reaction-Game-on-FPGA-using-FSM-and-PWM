1cd0b3cf47829f866424f8c87aa85f13f52aa44e
jrosas00 <jamesarosas101400@gmail.com>
2026-03-26 12:18:57 -0700

    Create Top module for FPGA button interface
    Initial module setup for FPGA button connection.

---
+++ Top.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: CSULB
+// Engineers: Michelle Herrera-Cuen, James Rosas
+//
+// Create Date: 03/26/2026 11:58:28 AM
+// Design Name: Final Project
+// Description: Connect FPGA to buttons
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top(
+
+    );
+
+    /*
+    INPUT: clk, btn[3:0]
+    OUTPUT: led[3:0]
+
+    instantiate ButtonInput
+
+    connect:
+        btn → ButtonInput
+        ButtonInput output → LEDs
+    */
+
+
+
+endmodule
