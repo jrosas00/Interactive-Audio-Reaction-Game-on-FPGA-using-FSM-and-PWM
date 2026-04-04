2a4eea31c3a60430080903b5ce7fce1b9664d53d
jrosas00 <jamesarosas101400@gmail.com>
2026-04-04 03:05:26 -0700

    ButtonInput instantiates multiple Button_Debouncer modules now
    ButtonInput instantiates multiple Button_Debouncer modules. One for each button(6). Input is clk from fpga and btn which is the 6 external buttons in the constraint file. Output is btn_press which comes from RISING_EDGE_PULSE from Button_Debouncer.v

--- ButtonInput.v
+++ ButtonInput.v
@@ -20,8 +20,52 @@
 
 */
 
-module ButtonInput(
-    input btn[5:0],
-    output btn_clean[5:0]);
+module ButtonInput (
+    input clk,
+    input  [5:0] btn,
+    output [5:0] btn_press // Declared as a wire. Output without reg is wire
+);
+
+    wire[5:0] btn_release; //add to module ButtonInput port list if used in TOP
     
+    Button_Debouncer b0(
+        .clk(clk),
+        .BUTTON(btn[0]),
+        .RISING_EDGE_PULSE(btn_press[0]),
+        .FALLING_EDGE_PULSE(btn_release[0])
+    );
+    
+     Button_Debouncer b1(
+        .clk(clk),
+        .BUTTON(btn[1]),
+        .RISING_EDGE_PULSE(btn_press[1]),
+        .FALLING_EDGE_PULSE(btn_release[1])
+    );
+     Button_Debouncer b2(
+        .clk(clk),
+        .BUTTON(btn[2]),
+        .RISING_EDGE_PULSE(btn_press[2]),
+        .FALLING_EDGE_PULSE(btn_release[2])
+    );
+     Button_Debouncer b3(
+        .clk(clk),
+        .BUTTON(btn[3]),
+        .RISING_EDGE_PULSE(btn_press[3]),
+        .FALLING_EDGE_PULSE(btn_release[3])
+    );
+     Button_Debouncer b4(
+        .clk(clk),
+        .BUTTON(btn[4]),
+        .RISING_EDGE_PULSE(btn_press[4]),
+        .FALLING_EDGE_PULSE(btn_release[4])
+    );
+     Button_Debouncer b5(
+        .clk(clk),
+        .BUTTON(btn[5]),
+        .RISING_EDGE_PULSE(btn_press[5]),
+        .FALLING_EDGE_PULSE(btn_release[5])
+    );
+
+   
+endmodule
    
