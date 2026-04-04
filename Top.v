e62844e740c982ce0b481abfaaa435b825ac0e9d
jrosas00 <jamesarosas101400@gmail.com>
2026-04-04 03:17:52 -0700

    Top.v instantiates ButtonInput and toggles LED based off button press
    Instantiates ButtonInput module and creates led_reg to hold last button press values to then output to onboard LED. This is needed because the LED would only turn on for a few ms which is not visible.

--- Top.v
+++ Top.v
@@ -11,20 +11,24 @@
 
 
 module Top(
+    input clk,
+    input [5:0] btn,
+    output [5:0] led
+);
 
+    wire [5:0] btn_press;
+    reg  [5:0] led_reg = 6'b0; // Register for holding button value when pressed to output to LED
+
+    // instantiate button module
+    ButtonInput b0(
+        .clk(clk),
+        .btn(btn),
+        .btn_press(btn_press)
     );
 
-    /*
-    INPUT: clk, btn[3:0]
-    OUTPUT: led[3:0]
-
-    instantiate ButtonInput
-
-    connect:
-        btn → ButtonInput
-        ButtonInput output → LEDs
-    */
-
-
-
+    always@(posedge clk)begin
+        led_reg <= led_reg ^ btn_press; // toggles led_reg based off button presses
+    end
+    assign led = led_reg; // Turns on LED's based off button press
+    
 endmodule
